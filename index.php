2990714a9079965d8342d5632941c393905ffdb5
Joshua Thomas <joshua.fujishima@gmail.com>
2017-01-07 12:08:34 -0500

    Created basic page structure.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Joshua Thomas - The Man A Yaad</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<style type="text/css">
+		
+	/*
+if you would like the <hr> to be full width, replace <div> with <div class='row'><div class='col-md-12'> (with according closing tags).
+
+	hr {
+	  -moz-border-bottom-colors: none;
+	  -moz-border-image: none;
+	  -moz-border-left-colors: none;
+	  -moz-border-right-colors: none;
+	  -moz-border-top-colors: none;
+	  border-color: #EEEEEE -moz-use-text-color #FFFFFF;
+	  border-style: solid none;
+	  border-width: 1px 0 0 0;
+	  margin: 18px 0;
+	}*/
+
+	</style>
+</head>
+<body>
+<div id="wrapper">
+	
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			
+		</div>
+	</nav>
+
+
+	<div id="main" class="container-fluid">
+		<div id="profile" class="row"></div>
+		<div id="portfolio" class="row"></div>
+		<div id="contact" class="row"></div>
+	</div>
+</div>
+
+<footer>
+	
+
+</footer>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
